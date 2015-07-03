7aa8518f933f7328dd8c922e41952b20015adf1b
LucasCecchi <lucascecchi2@gmail.com>
2015-07-02 18:56:01 -0600

    Finished everything except exception handling: It can fly but it can’t land

--- ImprovedReturnToBase/ViewController.m
+++ ImprovedReturnToBase/ViewController.m
@@ -14,9 +14,13 @@
 @end
 
 @implementation ViewController
-
+//Note that drone gets released from around 2m above the ground and thinks that is 0m
+const float HEIGHT = 10;
+const float SPEED_TO_RTB = 6;
+const float STAY_TIME = 3.0;
 double rcLatitude;
 double rcLongitude;
+bool isReturning = false;
 
 - (void)viewDidLoad
 {
@@ -28,6 +32,7 @@ double rcLongitude;
     [_drone connectToDrone];
     [_groundStation enterNavigationModeWithResult:^(DJIError *error) {}];
     _rc = _drone.remoteController;
+    //DJIGroundStationTask* RTBtask = [DJIGroundStationTask newTask];
 }
 
 - (void)didReceiveMemoryWarning
@@ -39,12 +44,31 @@ double rcLongitude;
     
 }
 - (IBAction)onSwitchRTB:(id)sender {
-    
+    isReturning =!isReturning;
+    if(!isReturning){
+    //add code to get rid of task to return home
+    }
 }
 
 -(void) remoteController:_rc didUpdateGpsData:(DJIRCGPSData)gpsData{
     rcLatitude = gpsData.mLatitude;
     rcLongitude = gpsData.mLongitude;
+    if(isReturning){
+        CLLocationCoordinate2D homePoint = {rcLatitude, rcLongitude};
+        if(CLLocationCoordinate2DIsValid(homePoint)){
+            DJIGroundStationWaypoint* homeWayPoint = [[DJIGroundStationWaypoint alloc]initWithCoordinate:homePoint];
+            homeWayPoint.altitude = HEIGHT;
+            homeWayPoint.horizontalVelocity = SPEED_TO_RTB;
+            homeWayPoint.stayTime = STAY_TIME;
+            DJIGroundStationTask* RTBtask = [DJIGroundStationTask newTask];
+            [RTBtask addWaypoint:homeWayPoint];
+            [_groundStation uploadGroundStationTask:RTBtask];
+            [_groundStation startGroundStationTask];
+        }
+        else{
+        //add error handling code here later
+        }
+    }
 }
 
 -(void) droneOnConnectionStatusChanged:(DJIConnectionStatus)status{
